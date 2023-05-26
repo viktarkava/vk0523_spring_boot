f398e34c5dc51458b70f7c6af94dce2ef476b41b
Viktar Kavalenka <viktar.kava@gmail.com>
2023-05-26 18:08:26 +0300

    Initial spring boot setup

---
+++ src/main/java/com/toolrental/toolRental/ToolRentalApplication.java
@@ -0,0 +1,13 @@
+package com.toolrental.toolRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolrental/toolRental/ToolRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolrental.toolRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
